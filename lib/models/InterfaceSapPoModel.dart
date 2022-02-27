5f4ad7c84bdd5e24736c50a343a5434dadc7c478
Traitet Thepbandansuk <traitet@hotmail.com>
2022-02-27 21:53:34 +0700

    model

--- lib/models/InterfaceSapPoModel.dart
+++ lib/models/InterfaceSapPoModel.dart
@@ -2,22 +2,34 @@ class InterfaceSapPoModel {
 //==============================================================================
 // DECLARE PROPERTY
 //==============================================================================
+  // ignore: non_constant_identifier_names
   String? RIDTTIFPO;
+  // ignore: non_constant_identifier_names
   String? PONUM;
+  // ignore: non_constant_identifier_names
   String? ITEMID;
+  // ignore: non_constant_identifier_names
   String? DOCTYPE;
+  // ignore: non_constant_identifier_names
   String? VENDORID;
+  // ignore: non_constant_identifier_names
   String? PAYMENTTERM;
 
 //==============================================================================
 // CONSTRUCTURE
 //==============================================================================
   InterfaceSapPoModel({
+    // ignore: non_constant_identifier_names
     this.RIDTTIFPO,
+    // ignore: non_constant_identifier_names
     this.PONUM,
+    // ignore: non_constant_identifier_names
     this.ITEMID,
+    // ignore: non_constant_identifier_names
     this.DOCTYPE,
+    // ignore: non_constant_identifier_names
     this.VENDORID,
+    // ignore: non_constant_identifier_names
     this.PAYMENTTERM,
   });
 
